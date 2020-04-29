87dcc252a4ddcce5a4ed2114ba3ad00cf891dafa
ComputerTechnology0 <64464330+ComputerTechnology0@users.noreply.github.com>
2020-04-30 04:45:14 +0500

    Add files via upload

---
+++ Python Random Generate Password/Random Password Generator.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+def password_generator(size = 8, chars = string.ascii_letters + string.digits + string.punctuation):
+    
+    return ''.join(random.choice(chars) for _ in range(size))
+
+print(password_generator(int(input('How many characters in your password? '))))
